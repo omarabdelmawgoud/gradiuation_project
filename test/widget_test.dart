3998d7e41e38e525f0f87cd963cf98f80b31ad18
Omar Abdelmawgoud <omarabdelmawgoud12@gmail.com>
2026-02-23 07:17:24 +0200

    scafold

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gradiuationg_project/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RVMS());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
